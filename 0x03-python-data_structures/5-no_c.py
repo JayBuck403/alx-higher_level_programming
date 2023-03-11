e95e2b8d92582c02c6b5f8afc2735a623e480b85
Theophilus Tay <jaybuck403@gmail.com>
2023-03-11 13:35:36 -0800

    You can't see me

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,4 +2,4 @@
 
 def no_c(my_string):
     return ''.join([character for character in my_string
-        if ord(character) != 99 and ord(character) != 67])
+                    if ord(character) != 99 and ord(character) != 67])
