24965af57d066fde42d44709a74501829d754ca6
Theophilus Tay <jaybuck403@gmail.com>
2023-03-28 08:06:17 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Create a square with size"""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size):
+        """Initialize a new square
+            
+        Args:
+            size (int): Size of new square 
+        """
+
+        self.__size = size
