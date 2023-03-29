119f74ce510f39bcb00fe46e98313a3a5a71ba5b
Theophilus Tay <jaybuck403@gmail.com>
2023-03-29 04:00:28 -0700

    Safe division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """Return division of a and b"""
+    try:
+        result = a / b
+    except (TypeError, ZeroDivisionError):
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
