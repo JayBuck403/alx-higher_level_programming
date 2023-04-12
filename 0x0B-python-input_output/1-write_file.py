9072a2f71eb540d32a233ee9fba837a645e01ab5
Theophilus Tay <jaybuck403@gmail.com>
2023-04-12 09:55:50 -0700

    Write to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-""""""
+"""write_file function"""
 
 
 def write_file(filename="", text=""):
+    """
+    Writes text to file
+    """
+
     with open(filename, 'w', encoding='utf-8') as my_file:
         return my_file.write(text)
