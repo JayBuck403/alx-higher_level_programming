879b5c15ace69ccbd7f4d459f0dc3ddd7b6d0e98
Theophilus Tay <jaybuck403@gmail.com>
2023-04-15 09:20:17 -0700

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Student Class"""
+
+
+class Student:
+    """
+    Creates a Student Class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student Object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student
+        instance  
+        """
+        return self.__dict__
