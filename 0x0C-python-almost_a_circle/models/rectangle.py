1368599f59aa354cc7ced0ff66ab39ef4d6a2074
Theo <jaybuck403@gmail.com>
2023-07-06 14:46:25 +0000

    Rectangle update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,11 +12,11 @@ class Rectangle(Base):
         """
         Initializes rectangle object
         """
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        super().__init__(id)
 
     @property
     def width(self):
